4a1ec81909a68bbf71a760a738f5861c5d0a7a42
mCyp <200522649@qq.com>
2019-08-11 22:34:26 +0800

    update

---
+++ ui/src/main/java/com/orient/ui/rv/TwoSideLayoutManager.java
@@ -0,0 +1,14 @@
+package com.orient.ui.rv;
+
+import android.support.v7.widget.RecyclerView;
+
+public class TwoSideLayoutManager extends RecyclerView.LayoutManager {
+
+
+
+
+    @Override
+    public RecyclerView.LayoutParams generateDefaultLayoutParams() {
+        return null;
+    }
+}
